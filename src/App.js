7ba33e5280b1d7fa2af2dcf2dfb880fa3e5a22b8
kayjee <kagiso.killagram@gmail.com>
2023-07-04 18:17:57 +0200

    added mission.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Missions from './pages/Missions';
+import MyProfile from './pages/Profile';
+import Rockets from './pages/Rockets';
+import Layout from './components/layOut';
+import NotMatch from './components/noMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App full-screen"> {/* Add the "full-screen" class */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="myprofile" element={<MyProfile />} />
+          <Route path="*" element={<NotMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/layOut.js
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
+
+const Layout = () => (
+  <>
+    <NavBar />
+    <Outlet />
+  </>
+);
+export default Layout;
