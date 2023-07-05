8e2ecabbb89fbcc306c25432e6353f0ea450fc6e
Derrick1451 <haylederrick14@gmail.com>
2023-07-05 13:39:26 +0300

    Fetch rockets from an external api

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,2 +1,24 @@
-const Rockets = () => <div>rockets content</div>;
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../features/rockets/rocketSlice';
+import RocketItem from './RocketItem';
+
+const Rockets = () => {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Something went wrong...</p>;
+  }
+  return (
+    <section className="container flex">
+      {rockets.map((rocket) => <RocketItem rocket={rocket} key={rocket.id} />)}
+    </section>
+  );
+};
 export default Rockets;
